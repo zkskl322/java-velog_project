2f4cb606a6f49493cad1c24a24d63e4abbc71e74
zkskl322 <dbawo530@gmail.com>
2024-07-26 17:48:05 +0900

    loginAndlogout

---
+++ src/main/java/com/example/blog/entity/Comment.java
@@ -0,0 +1,22 @@
+package com.example.blog;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private Post post;
+}
---
+++ src/main/java/com/example/blog/security/CustomUserDetailsService.java
@@ -0,0 +1,2 @@
+package com.example.blog.security;public class CustomUserDetailsService {
+}
